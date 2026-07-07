d444eb2e4a363ad00398675b0c54f6bd16bacfe1
Joel <joel.haden52@gmail.com>
2026-07-07 16:07:03 -0500

    Add bingo input form

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    
+    title=request.form['title']
+    songs = request.form['songs']
+    number_of_sheets = request.form['number_of_sheets']
+    
+    print('title:', title)
+    print('Number of sheets:', number_of_sheets)
+    print('Songs:')
+    print(songs)
+    
+    return 'Data received!'
 
 if __name__ == '__main__':
     app.run(debug=True)
